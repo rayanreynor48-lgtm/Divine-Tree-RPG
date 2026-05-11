431bd955eb1b14c07e8c572f3ec454067707dc07
rayanreynor48-lgtm <rayanreynor48@gmail.com>
2026-05-11 05:18:07 -0600

    Delete js directory

--- js/items.js
+++
@@ -1,1 +0,0 @@
-console.log('Items system loaded');
--- js/loading.js
+++
@@ -1,5 +0,0 @@
-window.onload = () => {
-    setTimeout(() => {
-        document.getElementById('loading-screen').style.display = 'none';
-    }, 3000);
-};
