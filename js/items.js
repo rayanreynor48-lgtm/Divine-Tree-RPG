3aa81bac2023fb1c30005b558cdb9a98da7ee70e
rayanreynor48-lgtm <rayanreynor48@gmail.com>
2026-05-11 05:15:37 -0600

    Add files via upload

---
+++ js/items.js
@@ -0,0 +1,1 @@
+console.log('Items system loaded');
---
+++ js/loading.js
@@ -0,0 +1,5 @@
+window.onload = () => {
+    setTimeout(() => {
+        document.getElementById('loading-screen').style.display = 'none';
+    }, 3000);
+};
